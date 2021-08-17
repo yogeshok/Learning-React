e77a172c489b8f510dd5a10c4772eebe59eed2a3
Yogesh Kumar <yogiky2002@gmail.com>
2021-08-18 00:29:35 +0530

    create-react-app 2 containers added

---
+++ learn-react/src/Header.js
@@ -0,0 +1,8 @@
+function Header(props) {
+
+    return (
+        <h1>Hi I'm {props.title}</h1>
+    )
+}
+
+export default Header;
---
+++ learn-react/src/Section.js
@@ -0,0 +1,9 @@
+function Section(props) {
+    return (
+        <h1>
+            Exercitation est minim quis sunt anim aliquip excepteur aliqua cupidatat reprehenderit velit. Magna et qui irure ea qui esse aliqua ex quis eiusmod. Ea in enim irure deserunt ad.
+        </h1>
+    )
+}
+
+export default Section;
