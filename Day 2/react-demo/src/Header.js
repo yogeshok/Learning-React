ddf9c75f0b67e42e2e52391b36804e6451465198
Yogesh Kumar <yogiky2002@gmail.com>
2021-08-19 11:07:21 +0530

    react-demo created, Link created between Header and Service

---
+++ Day 2/react-demo/src/Header.js
@@ -0,0 +1,11 @@
+import Service from "./Service";
+
+function Header() {
+    return (
+        <div>
+            <Service />
+        </div>
+    )
+}
+
+export default Header;
---
+++ Day 2/react-demo/src/Service.js
@@ -0,0 +1,19 @@
+function Service(props) {
+
+    return (
+        <div>
+            <div className="">
+                Image
+            </div>
+            <h1>Title</h1>
+            <p>
+                Description
+            </p>
+            <button>
+                Read more
+            </button>
+        </div>
+    )
+}
+
+export default Service;
